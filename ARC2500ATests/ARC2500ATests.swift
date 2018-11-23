d1c1710b33eeaa51425e05f5f4216de2817bb32d
john doe <johndoe@johns-mbp.attlocal.net>
2018-11-22 21:38:01 -0800

    Initial Commit

---
+++ ARC2500ATests/ARC2500ATests.swift
@@ -0,0 +1,34 @@
+//
+//  ARC2500ATests.swift
+//  ARC2500ATests
+//
+//  Created by john doe on 11/22/18.
+//  Copyright © 2018 Kirill Volchinskiy. All rights reserved.
+//
+
+import XCTest
+@testable import ARC2500A
+
+class ARC2500ATests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
